133b986c11d7af1c7bc6042e0ff59c766d446b2f
Thomas Burton <tburton5572@gmail.com>
2024-08-10 16:33:37 +0100

    setup testing environment

--- src/RPCF.jl
+++ src/RPCF.jl
@@ -1,3 +1,11 @@
 module RPCF
 
+using ReSolverInterface
+
+export points
+
+struct Grid <: AbstractGrid{Float64, 3} end
+ReSolverInterface.points(::Grid) = 5
+ReSolverInterface.volume(::Grid) = 1
+
 end
