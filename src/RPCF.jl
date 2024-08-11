4a94b276ef5ada0bc7ed82d96a93fae6632d4e6e
Thomas Burton <tburton5572@gmail.com>
2024-08-11 20:52:42 +0100

    basic implementation of grid (untested)

--- src/RPCF.jl
+++ src/RPCF.jl
@@ -2,10 +2,6 @@ module RPCF
 
 using ReSolverInterface
 
-export points
-
-struct Grid <: AbstractGrid{Float64, 3} end
-ReSolverInterface.points(::Grid) = 5
-ReSolverInterface.volume(::Grid) = 1
+include("grid.jl")
 
 end
---
+++ src/grid.jl
@@ -0,0 +1,32 @@
+# Definitions for the concrete implementation of the RPCF grid.
+
+struct RPCFGrid{S, DM<:AbstractMatrix} <: AbstractGrid{Float64, 3}
+    y::Vector{Float64}
+    Nz::Int
+    Nt::Int
+    domain::Vector{Float64}
+    Dy::DM
+    Dy2::DM
+    ws::Vector{Float64}
+
+    RPCFGrid(y, Nz, Nt, β, ω, Dy::DM, Dy2::DM, ws) where {DM} = new{(length(y), Nz, Nt), DM}(y, Nz, Nt, [β, ω], Dy, Dy2, ws)
+end
+
+# getter-setter methods
+function Base.getproperty(g::RPCFGrid, name::Symbol)
+    if name === :L
+        return 2π/g.domain[1]
+    elseif name === :β
+        return g.domain[1]
+    elseif name === :T
+        return 2π/g.domain[2]
+    elseif name === :ω
+        return g.domain[2]
+    else
+        return getfield(g, name)
+    end
+end
+
+# interface methods
+ReSolverInterface.points(g::RPCFGrid{S}) where {S} = (g.y, ntuple(i -> (0:(S[i + 1] - 1))/(S[i + 1])*(2π/g.domain[i]), 2)...)
+ReSolverInterface.volume(g::RPCFGrid) = 2*g.L*g.T
