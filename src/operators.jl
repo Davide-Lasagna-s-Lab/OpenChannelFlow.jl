d6434e94f6427af228a5d5fa2da2240dd852dd3f
Thomas Burton <tburton5572@gmail.com>
2024-08-21 21:31:31 +0100

    implemented RPCF RHS operators for objective computation

---
+++ src/operators.jl
@@ -0,0 +1,38 @@
+# Definitions of the RHS operators for RPCF.
+
+struct RPCFNavierStokesOperator{S, DM, DEALIAS, PAD, PLAN, IPLAN}
+    NSOper::NavierStokesOperator{3, RPCFField{S, DM, DEALIAS, PAD, PLAN, IPLAN}, Convergence!, Laplacian!}
+    Ro::Float64
+
+    RPCFNavierStokesOperator(g::RPCFGrid{S, DM, DEALIAS, PAD, PLAN, IPLAN}, Re::Float64, Ro::Float64) where {S, DM, DEALIAS, PAD, PLAN, IPLAN} = new{S, DM, DEALIAS, PAD, PLAN, IPLAN}(NavierStokesOperator(RPCFField, g, 3, Re, conv=Convection!(g), lapl=Laplacian!(g)), Ro)
+end
+
+function (f::RPCFNavierStokesOperator)(N_u::VectorField{3}, u::VectorField{3})
+    # compute standard NS operator
+    f.NSOper(N_u, u)
+
+    # compute cross product term
+    @. N_u[1] -= f.Ro*u[2]
+    @. N_u[2] += f.Ro*u[1]
+
+    return N_u
+end
+
+
+struct RPCFGradientOperator{S, DM, DEALIAS, PAD, PLAN, IPLAN}
+    GradOper::GradientOperator{3, RPCFField{S, DM, DEALIAS, PAD, PLAN, IPLAN}, Convection!, Convection2!, Laplacian!}
+    Ro::Float64
+
+    RPCFGradientOperator(g::RPCFGrid{S, DM, DEALIAS, PAD, PLAN, IPLAN}, Re::Float64, Ro::Float64) where {S, DM, DEALIAS, PAD, PLAN, IPLAN} = new{S, DM, DEALIAS, PAD, PLAN, IPLAN}(GradientOperator(RPCFField, g, 3, Re, conv1=Convection!(g), conv2=Convection2!(g), lapl=Laplacian!(g)), Ro)
+end
+
+function (f::RPCFGradientOperator)(M_ur::VectorField{3}, u::VectorField{3}, r::VectorField{3})
+    # compute standard gradient operator
+    f.GradOper(M_ur, u, r)
+
+    # compute cross product terms
+    @. M_ur[1] += f.Ro*u[2]
+    @. M_ur[2] -= f.Ro*u[1]
+
+    return M_ur
+end
