6de5e900bb7e1d1d516c62f972858eb5b2a7dc0c
Thomas Burton <tburton5572@gmail.com>
2024-08-28 22:17:10 +0100

    some testing of objective function constructor

--- src/RPCF.jl
+++ src/RPCF.jl
@@ -7,10 +7,12 @@ using ReSolverInterface
 export RPCFGrid, points, volume
 export RPCFField, FFT!, IFFT!, dot, norm, mult!
 export VectorField, grad!, divergence!, laplacian!, convection!, convection2!
+export Objective
 
 include("fft.jl")
 include("grid.jl")
 include("scalarfield.jl")
 include("vectorcalculus.jl")
+include("operators.jl")
 
 end
--- src/operators.jl
+++ src/operators.jl
@@ -1,10 +1,13 @@
 # Definitions of the RHS operators for RPCF.
 
-struct RPCFNavierStokesOperator{S, DM, DEALIAS, PAD, PLAN, IPLAN}
-    NSOper::NavierStokesOperator{3, RPCFField{S, DM, DEALIAS, PAD, PLAN, IPLAN}, Convergence!, Laplacian!}
+struct RPCFNavierStokesOperator{NSO}
+    NSOper::NSO
     Ro::Float64
 
-    RPCFNavierStokesOperator(g::RPCFGrid{S, DM, DEALIAS, PAD, PLAN, IPLAN}, Re::Float64, Ro::Float64) where {S, DM, DEALIAS, PAD, PLAN, IPLAN} = new{S, DM, DEALIAS, PAD, PLAN, IPLAN}(NavierStokesOperator(RPCFField, g, 3, Re, conv=Convection!(g), lapl=Laplacian!(g)), Ro)
+    function RPCFNavierStokesOperator(g::RPCFGrid, Re, Ro)
+        nso = NavierStokesOperator(RPCFField, g, 3, Re)
+        new{typeof(nso)}(nso, Float64.(Ro))
+    end
 end
 
 function (f::RPCFNavierStokesOperator)(N_u::VectorField{3}, u::VectorField{3})
@@ -19,11 +22,14 @@ function (f::RPCFNavierStokesOperator)(N_u::VectorField{3}, u::VectorField{3})
 end
 
 
-struct RPCFGradientOperator{S, DM, DEALIAS, PAD, PLAN, IPLAN}
-    GradOper::GradientOperator{3, RPCFField{S, DM, DEALIAS, PAD, PLAN, IPLAN}, Convection!, Convection2!, Laplacian!}
+struct RPCFGradientOperator{GRAD}
+    GradOper::GRAD
     Ro::Float64
 
-    RPCFGradientOperator(g::RPCFGrid{S, DM, DEALIAS, PAD, PLAN, IPLAN}, Re::Float64, Ro::Float64) where {S, DM, DEALIAS, PAD, PLAN, IPLAN} = new{S, DM, DEALIAS, PAD, PLAN, IPLAN}(GradientOperator(RPCFField, g, 3, Re, conv1=Convection!(g), conv2=Convection2!(g), lapl=Laplacian!(g)), Ro)
+    function RPCFGradientOperator(g::RPCFGrid, Re, Ro)
+        grad = GradientOperator(RPCFField, g, 3, Re)
+        new{typeof(grad)}(grad, Float64.(Ro))
+    end
 end
 
 function (f::RPCFGradientOperator)(M_ur::VectorField{3}, u::VectorField{3}, r::VectorField{3})
@@ -37,5 +43,4 @@ function (f::RPCFGradientOperator)(M_ur::VectorField{3}, u::VectorField{3}, r::V
     return M_ur
 end
 
-# TODO: test this
-ReSolverInterface.Objective(g::RPCFGrid, Re::Float64, Ro::Float64, modes::Array{ComplexF64, 4}, base::Vector{Float64}, free_mean::Bool=true) = ReSolverInterface.Objective(RPCFField, g, 3, Re, modes, base, free_mean, RPCFNavierStokesOperator(g, Re, Ro), RPCFGradientOperator(g, Re, Ro))
+Objective(g::RPCFGrid, Re::Real, Ro::Real, modes::Array{ComplexF64, 4}, base::Vector{Float64}, free_mean::Bool=true) = ReSolverInterface.Objective(RPCFField, g, 3, Re, modes, base, free_mean, RPCFNavierStokesOperator(g, Re, Ro), RPCFGradientOperator(g, Re, Ro))
