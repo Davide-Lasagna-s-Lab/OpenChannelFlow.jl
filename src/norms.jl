ff30d61d71e3aadab03b8b3d10c653c35cb14a15
Thomas Burton <tburton5572@gmail.com>
2025-11-10 13:12:21 +0000

    norms now sum to same base numeric type being used by fields

--- src/norms.jl
+++ src/norms.jl
@@ -3,8 +3,8 @@
 # ----------------------- #
 # standard inner products #
 # ----------------------- #
-function LinearAlgebra.dot(u::SCField{G}, v::SCField{G}) where {S, G<:ChannelGrid{S}}
-    sum = 0.0
+function LinearAlgebra.dot(u::SCField{G, T}, v::SCField{G, T}) where {S, G<:ChannelGrid{S}, T}
+    sum = zero(T)
     @loop_nt S[3] for ny in 1:S[1]
         @inbounds sum += grid(u).ws[ny]*real(dot(u[ny, 1, _nt], v[ny, 1, _nt]))
     end
@@ -14,16 +14,16 @@ function LinearAlgebra.dot(u::SCField{G}, v::SCField{G}) where {S, G<:ChannelGri
     return sum/2
 end
 
-function LinearAlgebra.dot(u::VectorField{N, <:SCField{G}}, v::VectorField{N, <:SCField{G}}) where {N, S, G<:ChannelGrid{S}}
-    sum = 0.0
+function LinearAlgebra.dot(u::VectorField{N, SCField{G, T}}, v::VectorField{N, SCField{G, T}}) where {N, S, G<:ChannelGrid{S}, T}
+    sum = zero(T)
     for n in 1:N
         @inbounds sum += dot(u[n], v[n])
     end
     return sum
 end
 
-function LinearAlgebra.dot(a::ProjectedField{G, M}, b::ProjectedField{G, M}) where {S, G<:ChannelGrid{S}, M}
-    sum = 0.0
+function LinearAlgebra.dot(a::ProjectedField{G, M, T}, b::ProjectedField{G, M, T}) where {S, G<:ChannelGrid{S}, M, T}
+    sum = zero(T)
     @loop_nt S[3] for m in 1:M
         @inbounds sum += real(dot(a[m, 1, _nt], b[m, 1, _nt]))
     end
@@ -39,8 +39,8 @@ LinearAlgebra.norm(u::Union{SCField, VectorField, ProjectedField}) = sqrt(dot(u,
 # ----------- #
 # other norms #
 # ----------- #
-function normdiff(u::SCField{G}, v::SCField{G}, shifts=(0, 0), tmp::SCField{G}=zero(v)) where {S, G<:ChannelGrid{S}}
-    sum = 0.0
+function normdiff(u::SCField{G, T}, v::SCField{G, T}, shifts=(0, 0), tmp::SCField{G, T}=zero(v)) where {S, G<:ChannelGrid{S}, T}
+    sum = zero(T)
     tmp .= v
     shift!(tmp, shifts)
     @loop_nt S[3] for ny in 1:S[1]
@@ -52,16 +52,16 @@ function normdiff(u::SCField{G}, v::SCField{G}, shifts=(0, 0), tmp::SCField{G}=z
     return sqrt(sum/2)
 end
 
-function normdiff(u::VectorField{N, <:SCField{G}}, v::VectorField{N, <:SCField{G}}, shifts=(0, 0), tmp::SCField{G}=zero(u[1])) where {N, S, G<:ChannelGrid{S}}
-    sum = 0.0
+function normdiff(u::VectorField{N, SCField{G, T}}, v::VectorField{N, SCField{G, T}}, shifts=(0, 0), tmp::SCField{G, T}=zero(u[1])) where {N, S, G<:ChannelGrid{S}, T}
+    sum = zero(T)
     for n in 1:N
         sum += normdiff(u[n], v[n], shifts, tmp)^2
     end
     return sqrt(sum)
 end
 
-function normdiff(a::ProjectedField{G, M}, b::ProjectedField{G, M}, shifts=(0, 0), tmp::ProjectedField{G, M}=zero(b)) where {S, G<:ChannelGrid{S}, M}
-    sum = 0.0
+function normdiff(a::ProjectedField{G, M, T}, b::ProjectedField{G, M, T}, shifts=(0, 0), tmp::ProjectedField{G, M, T}=zero(b)) where {S, G<:ChannelGrid{S}, M, T}
+    sum = zero(T)
     tmp .= b
     shift!(tmp, shifts)
     @loop_nt S[3] for m in 1:M
@@ -73,11 +73,11 @@ function normdiff(a::ProjectedField{G, M}, b::ProjectedField{G, M}, shifts=(0, 0
     return sqrt(sum/2)
 end
 
-function minnormdiff(u::Union{SCField{G}, VectorField{D, <:SCField{G}}, ProjectedField{G}},
-                     v::Union{SCField{G}, VectorField{D, <:SCField{G}}, ProjectedField{G}},
+function minnormdiff(u::Union{SCField{G}, VectorField{D, SCField{G, T}}, ProjectedField{G, M, T}},
+                     v::Union{SCField{G}, VectorField{D, SCField{G, T}}, ProjectedField{G, M, T}},
                      N::NTuple{2, Int}=(32, 32),
                   tmp1::SCField{G}=zero(v),
-                  tmp2::SCField{G}=zero(v)) where {D, G}
+                  tmp2::SCField{G}=zero(v)) where {D, G, M, T}
     # minimum values
     min_diff = Inf
     sz_min   = Inf
--- test/runtests.jl
+++ test/runtests.jl
@@ -6,5 +6,16 @@ using ChebUtils
 
 using OpenChannelFlow
 
+include("test_grid.jl")
+include("test_modenumber.jl")
+include("test_scfield.jl")
+include("test_pcfield.jl")
+include("test_vectorfield.jl")
+include("test_fft.jl")
+include("test_projectedfield.jl")
+include("test_broadcasting.jl")
+include("test_derivatives.jl")
+include("test_shifts.jl")
+include("test_norms.jl")
 include("test_couetteprimitive.jl")
 include("test_couettewallnormal.jl")
